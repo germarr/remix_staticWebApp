a03e218efae400537de97645d4be11be92b143d6
Gerardo Martinez <gerardomartinez@Gerardos-MacBook-Pro.local>
2022-08-08 19:53:32 -0400

    Removinng Supabase

--- app/root.tsx
+++ app/root.tsx
@@ -15,16 +15,6 @@ export const meta: MetaFunction = () => ({
   viewport: "width=device-width,initial-scale=1",
 });
 
-export const loader: LoaderFunction = () => {
-  return {
-    env: {
-      SUPABASE_URL: "https://tfatbvqvpnnftzcaoewb.supabase.co",
-      SUPABASE_KEY:
-        "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRmYXRidnF2cG5uZnR6Y2FvZXdiIiwicm9sZSI6ImFub24iLCJpYXQiOjE2NTg0MTM2MTcsImV4cCI6MTk3Mzk4OTYxN30.DDdCpzNrmsf7DYH0iCL8-YAvno2hsn_HKuFMk-g-OgM",
-    },
-  };
-};
-
 export default function App() {
   const { env } = useLoaderData();
   return (
@@ -35,11 +25,6 @@ export default function App() {
       </head>
       <body>
         <Outlet />
-        <script
-          dangerouslySetInnerHTML={{
-            __html: `window.env = ${JSON.stringify(env)}`,
-          }}
-        />
         <ScrollRestoration />
         <Scripts />
         <LiveReload />
